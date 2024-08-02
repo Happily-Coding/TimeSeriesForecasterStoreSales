6cb9bf9ac7e0b90c6c274a6cd4183092453c65ae
OnyxianSoul <onyxiansoul@gmail.com>
2024-08-02 12:44:58 -0300

    refactored create_db to use db_interface

--- data_engineering/create_db.py
+++ data_engineering/create_db.py
@@ -1,15 +1,4 @@
 #TODO, separate db creation and user creation? The user might already exist.
-import psycopg
-from sql_utils import create_engineering_user_if_not_exists_and_allowed, create_postgres_db
-from db_connector import db_name, db_host, db_port, db_admin_user, db_admin_password, db_data_engineer_user, db_data_engineer_password
-
-try:
-    create_postgres_db(db_host, db_port, db_admin_user, db_admin_password, db_name)
-    database_connection = psycopg.connect(f"host={db_host} port={db_port} user={db_admin_user} password={db_admin_password} dbname={db_name}")
-    create_engineering_user_if_not_exists_and_allowed(database_connection, db_data_engineer_user, db_data_engineer_password, db_name)
-    database_connection.close()
-except Exception as e:
-    print('There was a problem while connecting to the database server or creating the database:')
-    print(e)
-else:
-    print(f'Sucessfully created the postgres db: {db_name} at host={db_host} port={db_port} and the postgres user: {db_data_engineer_user}')
+from db_interfacing import db_interface
+db_interface.create_db()
+db_interface.create_engineering_user_if_not_exists_and_allowed()
